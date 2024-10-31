0a4eef5aca342d3c60f9799cfee5f10271653f5b
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-10-31 13:41:37 -0400

    feat(WeatherDisplay): render weather data

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -1,3 +1,25 @@
 import React from "react";
+import "./WeatherDisplay.css";
+function WeatherDisplay({ weatherData, forecastData, location }) {
+  if (!weatherData)
+    return <p className="loading-message">Loading weather data...</p>;
+  const description = weatherData.weather[0].description;
+  const icon = weatherData.weather[0].icon;
+  const temperature = Math.round((weatherData.main.temp * 9) / 5 + 32); // converts temp to Fahrenheit because AMERICA RAHHH
 
-function WeatherDisplay() {}
+  return (
+    <div className="weather-display">
+      <h1 className="location">{location}</h1>
+      <img
+        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+        alt={description}
+      />
+      <div>
+        <h2>{temperature}°F</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default WeatherDisplay;
