b84ac487c92718e72615f694a9f6204d4a6f3432
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-10-31 11:28:11 -0400

    feat(App): add data fetching for current weather and forecast data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,56 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import SearchBar from "../SearchBar/SearchBar";
 function App() {
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("Orlando");
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState([]);
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    if (!location) return;
+    const fetchWeatherData = async () => {
+      const API_KEY = process.env.REACT_APP_API_KEY;
+      try {
+        // fetch request for current weather
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
+        );
+        const data = await response.json();
+        setWeatherData(data);
+
+        // fetch request for forecast data
+        const forecastResponse = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=metric`
+        );
+        const forecastData = await forecastResponse.json();
+        setForecastData(forecastData);
+        setForecastData(forecastData.list.slice(0, 3)); // shows next 3 days forecast
+      } catch (error) {
+        console.error(`Error fetching weather data: ${error}`);
+      }
+    };
+
+    fetchWeatherData();
+  }, [location]);
+
+  const handleSearch = () => {
+    setLocation(query);
+  };
+
   return (
-    <div className="app">
-      <header className="app-header"></header>
+    <div className="App">
+      <header className="App-header"></header>
+      <body>
+        <div className="search">
+          <SearchBar
+            query={query}
+            setQuery={setQuery}
+            handleSearch={handleSearch}
+          />
+        </div>
+      </body>
     </div>
   );
 }
-
 export default App;
