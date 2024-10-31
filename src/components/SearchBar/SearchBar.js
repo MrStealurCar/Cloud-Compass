4291d93f4cfb691cf0a5aa3752f9d2264afe2126
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-10-31 11:29:29 -0400

    feat(SearchBar): import props and pass them to input and button elements

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,20 @@
 import React from "react";
-
-function SearchBar() {
-  return <input placeholder="Enter a city" className="search-bar" />;
+import "./SearchBar.css";
+function SearchBar({ query, setQuery, handleSearch }) {
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Enter a city"
+        className="search-bar"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button className="search-button" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
 }
 
 export default SearchBar;
