6c5a89b68280b6f59e69ad086a063fbf9fc46d6d
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-11-07 10:05:25 -0500

    feat(WeatherInfo): render 3 day forecast

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -1,5 +1,28 @@
 import React from "react";
 import "./WeatherInfo.css";
-function WeatherInfo() {}
+
+function WeatherInfo({ forecastData }) {
+  console.log(forecastData);
+  if (!forecastData || forecastData.length === 0) {
+    return <div>No forecast data available.</div>;
+  }
+
+  return (
+    <div className="weather-info">
+      {forecastData.map((day, index) => (
+        <div key={index} className="info">
+          <h3>{day.date}</h3>
+          <div>
+            <img
+              src={`http://openweathermap.org/img/wn/${day.icon}@2x.png`}
+              alt="weather icon"
+            />
+            <p>{Math.floor(day.temp)}°F</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default WeatherInfo;
