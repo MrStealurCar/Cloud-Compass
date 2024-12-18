60204da87347d76a4c4f474752ca55a68b02dc14
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-12-18 11:52:04 -0500

    feat(SearchResults): add search results

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,5 +1,19 @@
 import React from "react";
+import "./SearchResults.css";
+function SearchResults({ suggestions, onCitySelect, visible }) {
+  if (!visible || suggestions.length === 0) return null;
 
-function SearchResults() {}
+  return (
+    <div className="search-results">
+      <ul>
+        {suggestions.map((city, index) => (
+          <p key={index} onClick={() => onCitySelect(city)} className="city">
+            {city.name}, {city.country}
+          </p>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default SearchResults;
