cf48adb3d221743e4d60bb6874417ecae2b67523
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-01-20 14:20:25 -0500

    style(weatherDisplay): capitalize the first letter in city name

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./WeatherDisplay.css";
 function WeatherDisplay({ weatherData, location }) {
   if (!weatherData) return;
+  const city = location;
   const description = weatherData.weather[0].description;
   const icon = weatherData.weather[0].icon;
   const temperature = Math.round(weatherData.main.temp);
 
   return (
     <div className="weather-display">
-      <h1 className="location">{location}</h1>
+      <h1 className="location">
+        {city.charAt(0).toUpperCase() + city.slice(1)}
+      </h1>
       <img
         src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
         alt={description}
